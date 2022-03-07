7f1e10d3ea9116831ee3b2f63c00984214b85ca5
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-03-07 19:17:30 -0300

    primeiros exercicios

---
+++ Fundamentos/Bloco4-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,15 @@
+let estado = 7;
+
+switch (estado) {
+    case 1:
+        console.log('aprovada');
+        break;
+    case 2:
+        console.log('lista');
+        break;
+    case 3:
+        console.log('reprovada');
+        break;
+    default:
+        console.log('não se aplica')
+}
