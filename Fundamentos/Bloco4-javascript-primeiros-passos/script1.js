163399d9c67de8071101b2141605f1e077bde987
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-03-08 20:15:50 -0300

    realizado ate o exercicio 5

---
+++ Fundamentos/Bloco4-javascript-primeiros-passos/script1.js
@@ -0,0 +1,53 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+/* 1
+for (let number of numbers) {
+    console.log(number);
+} */
+
+/* 2
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+    sum += numbers[i];
+}
+
+console.log(sum); */
+
+/* 3 e 4
+let sum = 0
+let avg;
+for (let i = 0; i < numbers.length; i += 1) {
+    sum += numbers[i];
+    avg = sum / numbers.length;
+}
+console.log(avg); 
+
+if (avg > 20) {
+    console.log('valor maior que 20');
+}
+else console.log('valor menor ou igual a 20'); */
+
+/* 5
+let max = numbers[0];
+
+for (let i = 1; i < numbers.length; i += 1) {
+    if (max < numbers[i]) {
+        max = numbers[i]
+    }
+}
+console.log(max); */
+
+let odd = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] % 2 === !0) {
+        odd += 1;
+    }
+    else ;
+}
+
+if (odd === !0) {
+    console.log(odd);
+}
+else console.log('nenhum valor encontrado');
