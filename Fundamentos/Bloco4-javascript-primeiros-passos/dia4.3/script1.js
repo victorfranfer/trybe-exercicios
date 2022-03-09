71b7b99f11806328ba50333ea11a0bcdf3585cfe
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-03-09 20:01:19 -0300

    exercicio 4 feito

--- Fundamentos/Bloco4-javascript-primeiros-passos/dia4.3/script1.js
+++ Fundamentos/Bloco4-javascript-primeiros-passos/dia4.3/script1.js
@@ -36,6 +36,52 @@
 
 // console.log(longest);
 
+// 4
 
+// let primeNum = [];
+// let highestPrimeNum;
+
+// for (let i = 0; i < 50; i += 1) {
+//     if 
+//   primeNum.push(i + 1);
+// }
+// console.log(primeNum);
+
+function isPrime(num) {
+    for ( var i = 2; i < num; i++ ) {
+        if ( num % i === 0 ) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function display(n) {
+    var arr = [2];
+    for ( var i = 3; i < n; i+=2 ) {
+        if ( isPrime(i) ) {
+            arr.push(i);
+        }
+    }
+    console.log(arr); // use arr result on your own
+    let highest = arr[0];
+    for (let i = 0; i < arr.length; i += 1) {
+        if (highest < arr[i]) {
+            highest = arr[i];
+        }
+    }
+    console.log(highest);
+}
+
+display(50);
+
+
+
+// for (let i = 0; i < primeNum.length; i += 1) {
+//     if (primeNum[i] % primeNum[i += 1] === !0 && primeNum[i] % primeNum[i * 2 === !0]){
+//         highestPrimeNum = primeNum[i];
+//     }
+// }
+// console.log(highestPrimeNum);
 
 
