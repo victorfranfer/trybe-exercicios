6c1ffcd47c1c73a914c15abc56e012dbb5378d24
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-03-11 14:32:31 -0300

    script 3.js

--- Fundamentos/Bloco4-javascript-primeiros-passos/dia4.4/script3.js
+++ Fundamentos/Bloco4-javascript-primeiros-passos/dia4.4/script3.js
@@ -53,3 +53,48 @@
 // }
 
 // lowestValueIndex([2, 4, 6, 7, 10, 0, -3]);
+
+// EXERCICIO 4 INCOMPLETO - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres
+
+// function longestName(array) {
+//   // if (array.length === 0) {
+//   //   return -1;
+//   // }
+  
+//   for (let i = 0; i < array.length; i += 1) {
+//     firstName = array[0].split("");
+//   let longest = firstName.length;
+//     let name = array[i].split("");
+//     if (name.length > longest) {
+//       longest = array[i];
+//     }
+//     console.log(longest);
+//   }
+  
+// }
+
+// longestName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']);
+
+// EXERCICIO 5 INCOMPLETO - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete;
+
+// function mode(array) {
+//   return array.sort((a, b) =>
+//     array.filter(v => v===a).length
+//     - array.filter(v => v===b).length
+//   ).pop();
+//   console.log(arra)
+// }
+
+// mode([2, 3, 2, 5, 8, 2, 3])
+
+// EXERCICIO 6 - Crie uma função que receba um número natural (inteiro não negativo) N e retorne o somatório de todos os os números de 1 até N.
+
+// function sumOneToN(N) {
+//   let array =
+// }
+
+// EXERCICIO 7 - Crie uma função que receba uma string word e outra string ending. Verifique se a string ending é o final da string word. Ending sempre será menor que word.
+
+// function (word, ending) {
+//   if (word.length <= ending.length)
+// }
