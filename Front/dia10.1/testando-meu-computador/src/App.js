0cb4b434aeac8e520d234121c7b10ffed52b9507
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-07-29 19:00:26 -0300

    a

---
+++ Front/dia10.1/testando-meu-computador/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span>Hello, World!</span>
+      <p>I am not very happy because I haven't been able to finish my last project yet.</p>
+      <p>What do you recommend me doing to solve this situation?</p>
+      <p>I am talking to myself via a React App and showing my messages to myself on my Web Browser</p>
+      <p>How crazy am I?</p>
+      <p>At least it's my first React App...</p>
+    </div>
+  );
+}
+
+export default App;
