7ff31682212b5080e5deb12c2dff94d36f5b0aab
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-02-14 13:04:27 -0300

    exercise 6 - day 1

--- CS/Introducao_a_python/dia01/playing_with_numbers.py
+++ CS/Introducao_a_python/dia01/playing_with_numbers.py
@@ -31,7 +31,7 @@ def print_asteriscs(number):
             print(number * '*')
 
 
-print_asteriscs(8)
+print_asteriscs(2)
 
 
 # Exercise 4: Create a function that receives a list of names and returns the name with the most characters
@@ -59,3 +59,24 @@ def paint_costs(area):
     print(required_cans, required_cans * can_price)
 
 paint_costs(500)
+
+
+# Exercise 6: Write a function that receives the sides of a triangle and tell what kind of triangle it is
+
+def type_of_triangle(side1, side2, side3):
+    is_triangle = (
+            side1 + side2 > side3 and
+            side2 + side3 > side1 and
+            side1 + side3 > side2
+    )
+    if not is_triangle:
+        return "não é triângulo"
+    elif side1 == side2 == side3:
+        return "equilátero"
+    elif side1 == side2 or side2 == side3 or side1 == side3:
+        return "isósceles"
+    else:
+        return "escaleno"
+    
+
+type_of_triangle(5, 4, 3)
