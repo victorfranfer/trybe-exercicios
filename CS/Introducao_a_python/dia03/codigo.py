187309b62c1d78a10f2d23d227752f221933a107
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-02-15 16:27:24 -0300

    day3 - pre-class

---
+++ CS/Introducao_a_python/dia03/codigo.py
@@ -0,0 +1,8 @@
+def is_odd(number):
+    'Retorna True se um número é impar, se não False.'
+    return number % 2 != 0
+
+
+def divide(a_number, other_number):
+    "Retorna a divisão de a_number por other_number"
+    return a_number / other_number
