123819c7f2a9389227ea3df4ad42258795d054b4
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-03-08 13:49:39 -0300

    add_raspagem_de_dados

---
+++ CS/Raspagem_de_dados/ratelimit.py
@@ -0,0 +1,23 @@
+import requests
+import time
+
+
+# Coloca uma pausa de 6 segundos a cada requisição
+# Obs: este site de exemplo tem um rate limit de 10 requisições por minuto
+for _ in range(15):
+    response = requests.get("https://www.cloudflare.com/rate-limit-test/")
+    print(response)
+    time.sleep(6)
+
+# Timeout
+# Por 10 segundos não temos certeza se a requisição irá retornar
+response = requests.get("https://httpbin.org/delay/10")
+print(response)
+try:
+    # recurso demora muito a responder
+    response = requests.get("http://httpbin.org/delay/10", timeout=2)
+except requests.ReadTimeout:
+    # vamos fazer uma nova requisição
+    response = requests.get("http://httpbin.org/delay/1", timeout=2)
+finally:
+    print(response.status_code)
